8d5efacaffd76fe53c894856a29e4a9d0e37ea73
WiFixAion <161218080+WiFixAion@users.noreply.github.com>
2024-04-25 01:11:06 +0800

    Update on Buttons
    Update

--- framework/users-module/create-user.php
+++ framework/users-module/create-user.php
@@ -26,10 +26,6 @@
         margin-bottom: 20px;
     }
 
-    #button-block {
-        text-align: center;
-    }
-
     button {
         background-color: #4CAF50;
         color: white;
@@ -87,6 +83,63 @@
   --option-bg: black;
   --select-bg: rgba(255, 255, 255, 0.2);
 }
+.button {
+  position: relative;
+  width: 170px;
+  height: 40px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  border: 1px solid #34974d;
+  background-color: #3aa856;
+}
+
+.button, .button__icon, .button__text {
+  transition: all 0.3s;
+}
+
+.button .button__text {
+  transform: translateX(30px);
+  color: #fff;
+  font-weight: 600;
+}
+
+.button .button__icon {
+  position: absolute;
+  transform: translateX(109px);
+  height: 100%;
+  width: 39px;
+  background-color: #34974d;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.button .svg {
+  width: 30px;
+  stroke: #fff;
+}
+
+.button:hover {
+  background: #34974d;
+}
+
+.button:hover .button__text {
+  color: transparent;
+}
+
+.button:hover .button__icon {
+  width: 148px;
+  transform: translateX(0);
+}
+
+.button:active .button__icon {
+  background-color: #2e8644;
+}
+
+.button:active {
+  border: 1px solid #2e8644;
+}
 </style>
 <?php if (isset($_GET['errmsg'])) { echo "<p style='color: red'>" . htmlspecialchars($_GET['errmsg']) . "</p>"; } ?>
 <h3>Provide the Required Information</h3>
@@ -131,8 +184,10 @@
             <!-- <label for="username">Username</label>
             <input type="text" autocomplete="off" name="username" class="input" placeholder="Username"> -->
         </div>
-        <div id="button-block">
-            <input type="submit" value="Save">
-        </div>
+        
+<button type="button" class="button">
+  <span class="button__text">Save</span>
+  <span class="button__icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24" stroke-width="2" stroke-linejoin="round" stroke-linecap="round" stroke="currentColor" height="24" fill="none" class="svg"><line y2="19" y1="5" x2="12" x1="12"></line><line y2="12" y1="12" x2="19" x1="5"></line></svg></span>
+</button>
     </form>
 </div>
